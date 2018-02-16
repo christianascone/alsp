2f34508cf0f0880a323c45c4074be89c4b54bd20
ChristianAscone <ascone.christian@gmail.com>
2018-02-16 09:30:55 +0100

    Set parameters from command-line with commander and set bin file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+var program = require('commander');
+
 const http = require('http');
 const { URL } = require('url');
 
@@ -15,48 +18,58 @@ const _request = url => new Promise((resolve, reject) => {
   }).on('error', reject);
 });
 
-module.exports = (options = {
+program
+.option('-H, --host <host>', 'Set host (default 0.0.0.0)')
+.option('-P, --port <port>', 'Set port (default 8997)')
+.option('-e, --endpoint <endpoint>', 'Set endpoint (default http://localhost)')
+.parse(process.argv);
+
+options = {
   host: '0.0.0.0',
   port: 8997,
   server: 'http://localhost',
-}) => {
-  console.info('Starting server...');
-
-  const server = http.createServer(async (req, res) => {
-    console.info('Redirecting...');
-
-    try {
-      const url = new URL(req.url, options.server).toString();
-      const { statusCode, headers, body } = await _request(url);
-
-      res.writeHead(statusCode, headers);
-      res.write(body);
-      res.end();
-
-      console.info('Ok');
-    } catch (err) {
-      console.error('Request error', err);
-
-      res.writeHead(500, err.message);
-      res.write(err.message);
-      res.end();
-    }
-  }).listen(options.port, options.host);
-
-  const stop = () => {
-    console.info('Stopping server...');
-    server.close(() => process.exit(0));
-
-    setTimeout(() => {
-      process.exit(0);
-    }, 5000); // Force exit
-  };
-
-  server.on('listening', () => {
-    console.info(`Redirecting requests to ${options.server}`);
-    console.info(`Listening on http://${options.host}:${options.port} <- Use this url to be redirected to given endpoint`);
-  });
-
-  process.on('SIGTERM', stop);
-  process.on('SIGINT', stop);
 };
+
+if(program.host) options.host = program.host;
+if(program.port) options.port = program.port;
+if(program.endpoint) options.server = program.endpoint;
+
+console.info('Starting server...');
+
+const server = http.createServer(async (req, res) => {
+  console.info('Redirecting...');
+
+  try {
+    const url = new URL(req.url, options.server).toString();
+    const { statusCode, headers, body } = await _request(url);
+
+    res.writeHead(statusCode, headers);
+    res.write(body);
+    res.end();
+
+    console.info('Ok');
+  } catch (err) {
+    console.error('Request error', err);
+
+    res.writeHead(500, err.message);
+    res.write(err.message);
+    res.end();
+  }
+}).listen(options.port, options.host);
+
+const stop = () => {
+  console.info('Stopping server...');
+  server.close(() => process.exit(0));
+
+  setTimeout(() => {
+    process.exit(0);
+    }, 5000); // Force exit
+};
+
+server.on('listening', () => {
+  console.info(`Redirecting requests to ${options.server}`);
+  console.info(`Listening on http://${options.host}:${options.port} <- Use this url to be redirected to given endpoint`);
+});
+
+process.on('SIGTERM', stop);
+process.on('SIGINT', stop);
